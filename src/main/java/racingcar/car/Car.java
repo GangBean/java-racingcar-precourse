a1766768d6a35a3a3de629e5461d7f8d1548de55
Gangbean <xbox2537@gamil.com>
2022-04-23 16:03:12 +0900

    feat : Car 기능 구현 -> 테스트 정상화
    - 자동차 생성 기능
    - 자동차 이동거리 생성 기능
    - 자동차 이동여부 판단 기능
    - 자동차 이동 기능

---
+++ src/main/java/racingcar/car/Car.java
@@ -0,0 +1,49 @@
+package racingcar.car;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Car {
+    public static final int MIN_MOVE_COUNT = 0;
+    public static final int MAX_MOVE_COUNT = 9;
+    public static final int MOVE_STANDARD_COUNT = 4;
+    public static final boolean MOVE = true;
+    private String name;
+    private int moveCount;
+    private boolean moveOrStop;
+    private int carPosition;
+
+    public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getMoveCount() {
+        return this.moveCount;
+    }
+
+    public void setMoveCount() {
+        this.moveCount = Randoms.pickNumberInRange(MIN_MOVE_COUNT, MAX_MOVE_COUNT);
+    }
+
+    public boolean getMoveOrStop() {
+        this.moveOrStop = (this.moveCount>= MOVE_STANDARD_COUNT);
+        return this.moveOrStop;
+    }
+
+    public int getCarPosition() {
+        return this.carPosition;
+    }
+
+    public void moveCar() {
+        if (this.moveOrStop==MOVE) {
+            this.carPosition += this.moveCount;
+        }
+        this.moveCount = 0;
+    }
+}
